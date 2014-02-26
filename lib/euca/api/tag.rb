d28dd6dca0098cd3320efe8f47e3befce82442c3
Onur Uyar <me@onuruyar.com>
2014-02-26 10:26:29 +0000

    fix for variable column count on tag api

--- lib/euca/api/tag.rb
+++ lib/euca/api/tag.rb
@@ -5,14 +5,22 @@ module Euca
     include Euca::Filterable
           
     TYPE_ID = "tag"
-    TYPE_ATTRS = %w(type instance_id key value)
+    TYPE_ATTRS = %w(type instance_id instance_type key value)
       
     def self.create key, value, instance_id = nil
-      if key.is_a? Hash
-        euca("create-tags", key.map{|key,value| "--tag #{key}=#{value}" }.join(" ") , value).first
-      else
-        return nil if instance_id.nil?
-        euca("create-tags", "--tag #{key}=#{value}", instance_id).first
+      result = begin
+        if key.is_a? Hash
+          euca("create-tags", key.map{|key,value| "--tag #{key}=#{value}" }.join(" ") , value).first
+        else
+          return nil if instance_id.nil?
+          euca("create-tags", "--tag #{key}=#{value}", instance_id).first
+        end
+      end
+      if result
+        result['value']=result['key']
+        result['key']=result['instance_type']    
+        result.delete('instance_type')  
+        result
       end
     end
       
